31d0017924e11952df608e44dbcff8ed04327c2c
sudeeproy520 <sudeeproy520@gmail.com>
2024-02-13 19:57:49 +0530

    flask second some updates

--- flask/app.py
+++ flask/app.py
@@ -1,15 +1,40 @@
-from flask import Flask, jsonify, render_template, request, redirect
+from flask import Flask, jsonify, render_template, request, redirect, flash, url_for
+import os
+from werkzeug.utils import secure_filename
 
-
+UPLOAD_FOLDER = './static/upload'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__, template_folder="templates")
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route("/")
 def homepge():
     return index()
     # return scan()
 
-@app.route("/index")
+@app.route("/index", methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.fileUpload:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.fileUpload['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return render_template("index.html")
 
 @app.route("/aboutUs")
